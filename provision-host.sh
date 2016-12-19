54f33a4bab63e9f61b0ac03235946b25e0ea5ade
Eli Mallon <eli@stream.kitchen>
2016-12-18 21:15:55 -0800

    Upgrade Ubuntu one to 16.04

--- provision-host.sh
+++ provision-host.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
 # Only prerequisites: docker and curl. And the assumption that you're running on an EC2 instance. And that your name
 # is Eli Mallon and you're running on my AWS account, that's a big one too.
+
+# Close STDOUT file descriptor
+exec 1<&-
+# Close STDERR FD
+exec 2<&-
+
+# Open STDOUT as $LOG_FILE file for read and write.
+exec 1<>/var/log/drumstick-eli.log
+
+# Redirect STDERR to STDOUT
+exec 2>&1
+
+apt update && apt -y install awscli linux-image-extra-$(uname -r) linux-image-extra-virtual  apt-transport-https ca-certificates
+
+apt-key adv \
+               --keyserver hkp://ha.pool.sks-keyservers.net:80 \
+               --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+apt update
+apt-get install -y docker-engine
+service docker start
 
 # Set the host's SSH port to 2222
 cat /etc/ssh/sshd_config | sed s/"^Port 22$"/"Port 2222"/ > /etc/ssh/sshd_config_new
@@ -14,31 +40,28 @@ instanceId=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 # Mount the EBS with my home directory on it
 export AWS_DEFAULT_REGION=us-west-2
 aws ec2 attach-volume \
-  --volume-id vol-58d929be \
+  --volume-id vol-8253c875 \
   --instance-id `ec2metadata --instance-id`\
   --device /dev/xvdb
-mkdir -p /mnt/user
+mkdir -p /home/root
 while [ ! -e /dev/xvdb ]
 do
   sleep 1
 done
-mount /dev/xvdb /mnt/user
-
-# Disable apparmor for the container. We can do whatever we want.
-aa-complain /etc/apparmor.d/docker
+mount /dev/xvdb /home/root
 
 # Gimmie my elastic IP
 aws ec2 associate-address \
   --allocation-id eipalloc-c54dfca0 \
   --instance-id `ec2metadata --instance-id`
 
-# Docker please.
-curl -sSL https://get.docker.com/ | sh
-service docker start
-
 # Fire up the real thing!
 cat <<EOF > /root/start.sh
 #!/bin/bash
+exec 1<&-
+exec 2<&-
+exec 1<>/var/log/drumstick-eli-run.log
+exec 2>&1
 while true; do
   docker pull iameli/drumstick-eli
   docker run \
@@ -46,7 +69,7 @@ while true; do
     --rm \
     --name drumstick \
     --net host \
-    -v /mnt/user:/root \
+    -v /home/root:/home/root \
     -v /run/docker.sock:/run/docker.sock \
     iameli/drumstick-eli
   sleep 1
